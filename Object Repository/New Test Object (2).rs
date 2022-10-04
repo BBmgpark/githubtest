ac13a648084b4fe4385f87551357da42af403a35
BBmgpark <110453550+BBmgpark@users.noreply.github.com>
2022-10-04 10:34:42 +0900

    test2

---
+++ Object Repository/New Test Object (2).rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object (2)</name>
+   <tag></tag>
+   <elementGuidId>6c2b9d09-bf16-42e1-ad49-4467e9ec7f4e</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
