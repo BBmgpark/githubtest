93789a9165528f9036b6320143fc86a0fc6a6711
BBmgpark <110453550+BBmgpark@users.noreply.github.com>
2022-10-04 10:08:43 +0900

    test2

---
+++ Object Repository/New Test Object.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>1111</description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>80628bdb-3246-434f-9966-8743b3b46b36</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>22222</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
